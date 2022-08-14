2164bbf2428f16d53f60a58d15bc5710be21d06a
m-holovan <nikita2003n@gmail.com>
2022-08-14 14:22:22 +0300

    firstCommit

---
+++ Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        // address and port of the server to which we will connect
+        static int port = 8005;//server port
+        static string address = "127.0.0.1";//server address
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                //connect to remote host
+                socket.Connect(ipPoint);
+                Console.WriteLine("Enter message:");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                //get answer
+                data = new byte[256];//response buffer
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;//number of received bytes
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                Console.WriteLine("server response:" + builder.ToString());
+
+                //close socket
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static int port = 8005; //port for receiving incoming requests 
+        static void Main(string[] args)
+        {
+            //get socket start addresses
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            //create socket
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                //we connect the socket with a local point on which we will receive data
+                listenSocket.Bind(ipPoint);
+
+                //start listening
+                listenSocket.Listen(10);
+
+                Console.WriteLine("The server is running. Waiting for connections...");
+
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    //get message
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; //number of bytes received
+                    byte[] data = new byte[256];//buffer for received data
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                    //send answer
+                    string message = "Your message has been delivered";
+                    data = Encoding.Unicode.GetBytes(message);
+                    handler.Send(data);
+                    //close socket
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
